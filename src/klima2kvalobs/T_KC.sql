9ce921e644c7ef6580deca2bc70fc25173c6a44e
Terje Erlend Reite <terje.reite@met.no>
2010-04-27 14:04:44 +0000

    git-svn-id: https://svn.met.no/kvoss/kvalobs_metadata/branches/autotools@1505 422d51e2-5941-41ac-8e65-d5b5931239be

---
+++ src/klima2kvalobs/T_KC.sql
@@ -0,0 +1,6 @@
+spool on
+spool T_KC.lst
+set heading off
+set space 1 lines 500
+select STNR||','||ELEM_CODE||','||FYEAR||','||FMONTH||','||TYEAR ||','||TMONTH ||','||JAN ||','||FEB ||','||MAR ||','||APR||','||MAY||','||JUN||','||JUL||','||AUG||','||SEP||','||OCT||','||NOV ||','||DEC||','||TO_CHAR(FDATO,'YYYY-MM-DD')||','||TO_CHAR(TDATO,'YYYY-MM-DD') from T_KC;
+spool off 
