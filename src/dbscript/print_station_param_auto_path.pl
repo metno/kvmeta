cdd4bd9152881d77ff68d6ca4f7d077533ea4314
Terje Erlend Reite <terje.reite@met.no>
2013-02-11 12:53:49 +0000

    git-svn-id: https://svn.met.no/kvoss/kvalobs_metadata/trunk@4273 422d51e2-5941-41ac-8e65-d5b5931239be

---
+++ src/dbscript/print_station_param_auto_path.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+# Kvalobs_Metadata - Free Quality Control Algorithms for Meteorological Observations 
+#
+# $Id: dbQC1-3 263 2008-03-18 11:43:57Z vegardb $
+#
+# Copyright (C) 2007 met.no
+#
+# Contact information:
+# Norwegian Meteorological Institute
+# Box 43 Blindern
+# 0313 OSLO
+# NORWAY
+# email: kvalobs-dev@met.no
+#
+# This file is part of KVALOBS_METADATA
+# 
+# KVALOBS_METADATA is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as 
+# published by the Free Software Foundation; either version 2 
+# of the License, or (at your option) any later version.
+#
+# KVALOBS_METADATA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with KVALOBS_METADATA; if not, write to the Free Software Foundation Inc., 
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+use POSIX;
+use strict;
+use dbQC;
+
+print get_station_param_path();
