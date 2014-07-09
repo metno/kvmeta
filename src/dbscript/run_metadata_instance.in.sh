3634c644c36c8ad674a031263273b801f69437de
Terje Erlend Reite <terje.reite@met.no>
2014-07-09 13:37:42 +0000

    git-svn-id: https://svn.met.no/kvoss/kvalobs_metadata/trunk@5128 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/dbscript/run_metadata_instance.in.sh
+++ src/dbscript/run_metadata_instance.in.sh
@@ -66,8 +66,9 @@ echo "Sletter tabellene metadatatype og station_metadata"
 $PSQL -a -c "TRUNCATE metadatatype CASCADE"
 $PSQL -a -c "\copy metadatatype from '$METADIR/metadatatype/metadatatype.out' DELIMITER '|'"
 
-echo "Oppdaterer tabellene station types param obs_pgm station_metadata model qcx_info operator"
-for TABLE in station types param obs_pgm station_metadata model qcx_info operator
+
+echo "Oppdaterer tabellene station types param station_metadata model qcx_info operator"
+for TABLE in station types param station_metadata model qcx_info operator
 do
     $PSQL -c "\copy $TABLE to $DUMPDIR/$TABLE.out DELIMITER '|'"
        
@@ -78,29 +79,34 @@ do
     fi
 done
 
-for INSTANCE in $INSTANCE_LIST
-do
-   echo "Oppdaterer tabellen obs_pgm for instansen $INSTANCE"
-   for TABLE in obs_pgm
-   do
-      if [ -n "$INSTANCE" ]; then
-         TABLE_INSTANCE=${TABLE}_${INSTANCE}
-      else
-         TABLE_INSTANCE=${TABLE}
-      fi
 
-      echo "TABLE_INSTANCE=$TABLE_INSTANCE"
-      $PSQL -d $INSTANCE -c "\copy $TABLE to $DUMPDIR/$TABLE_INSTANCE.out DELIMITER '|'"
-       
-      if ! diff -q  $DUMPDIR/$TABLE_INSTANCE.out $METADIR/$TABLE_INSTANCE/$TABLE_INSTANCE.out; then
-         # $LIBEXECDIR/run_$TABLE $INSTANCE
-         $PSQL -d $INSTANCE -a -c "truncate $TABLE"
-         $PSQL -d $INSTANCE -a -c "\copy $TABLE from $METADIR/$TABLE_INSTANCE/$TABLE_INSTANCE.out DELIMITER '|'" 		
-         echo -e `date` "\t$TABLE updated" >> $LOGFILE
-         assert_table_not_empty $TABLE
-      fi
-   done
-done
+#for INSTANCE in $INSTANCE_LIST METNO
+#do
+#   echo "Oppdaterer tabellen obs_pgm for instansen $INSTANCE"
+#   TABLE="obs_pgm"
+#      if [ -n "$INSTANCE" ]; then
+#         TABLE_INSTANCE=${TABLE}_${INSTANCE}
+#      else
+#         TABLE_INSTANCE=${TABLE}
+#      fi
+#
+#      if [ "$INSTANCE" =  "METNO" ]; then
+#         TABLE_INSTANCE=${TABLE}
+#      fi
+#
+#      echo "TABLE_INSTANCE=$TABLE_INSTANCE"
+#      $PSQL -d $INSTANCE -c "\copy $TABLE to $DUMPDIR/$TABLE_INSTANCE.out DELIMITER '|'"
+#       
+#      if ! diff -q  $DUMPDIR/$TABLE_INSTANCE.out $METADIR/$TABLE_INSTANCE/$TABLE_INSTANCE.out; then
+#         # $LIBEXECDIR/run_$TABLE $INSTANCE
+#         $PSQL -d $INSTANCE -a -c "truncate $TABLE"
+#         $PSQL -d $INSTANCE -a -c "\copy $TABLE from $METADIR/$TABLE_INSTANCE/$TABLE_INSTANCE.out DELIMITER '|'" 		
+#         echo -e `date` "\t$TABLE updated" >> $LOGFILE
+#         assert_table_not_empty $TABLE
+#      fi
+# 
+#done
+
 
 echo "Oppdaterer tabellene  algorithms checks station_param"
 for TABLE in algorithms checks station_param
@@ -122,11 +128,14 @@ done
 
 $PSQL -a -c "\copy station_param from '$METADIR/station_param/station_param_auto/QC1-1_all.out' DELIMITER '|'" 
 
+
 # Table station_param need several scripts for updating
 for COMMAND in  "run_QC1-3 all" "run_QC1-4 all"
 do
     $LIBEXECDIR/$COMMAND
+   
 done
+
 
 echo "$LIBEXECDIR/station_param2kvalobsdb station_param_QCX.out nonhour > $DUMPDIR/station_param_QCX.log"
 $LIBEXECDIR/station_param2kvalobsdb station_param_QCX.out nonhour > $DUMPDIR/station_param_QCX.log
@@ -146,80 +155,133 @@ $LIBEXECDIR/checks2kvalobsdb checks_qcx.out > $DUMPDIR/checks_qcx.log
 
 for COMMAND in run_algorithm_all run_station_param_all  run_checks_all
 do
-    echo "$LIBEXECDIR/$COMMAND > $DUMPDIR/$COMMAND.out"
-    $LIBEXECDIR/$COMMAND > $DUMPDIR/$COMMAND.out
+      echo "$LIBEXECDIR/$COMMAND > $DUMPDIR/$COMMAND.out"
+      $LIBEXECDIR/$COMMAND > $DUMPDIR/$COMMAND.out
 done  
 
-####
+
+#### MAKING METADATA PACKAGES ####
 METADIST=`$KVCONFIG --datadir`/kvalobs/metadist
 mkdir -p -m700 "$METADIST/kvmeta"
-echo "Sjekker antall linjer i tabellene og dumper tabellene"
-for TABLE in algorithms checks station_param station types param obs_pgm metadatatype station_metadata model qcx_info operator
+#echo "Sjekker antall linjer i tabellene og dumper tabellene"
+#for TABLE in algorithms checks station_param station types param obs_pgm metadatatype station_metadata model qcx_info operator
+#do
+#    assert_table_not_empty $TABLE
+#    $PSQL -c "\copy $TABLE to $METADIST/kvmeta/$TABLE.out DELIMITER '|'"
+#done
+#
+#assert_table_not_empty station
+#$PSQL -c "\copy ( select * from station  where static='t' ) to $METADIST/kvmeta/station.out DELIMITER '|'"
+#
+#cd $METADIST
+#kvmetadist=kvmeta-$(date +%Y%m%d).tar.bz2
+#
+#CLIENT_ENCODING=`$PSQL -tc "SHOW CLIENT_ENCODING"| tr -d ' '`
+#SERVER_ENCODING=`$PSQL -tc "SHOW SERVER_ENCODING"| tr -d ' '`
+#echo "CLIENT_ENCODING=${CLIENT_ENCODING}"
+#echo "SERVER_ENCODING=${SERVER_ENCODING}"
+#
+### CHARSET CONVERSION FOR UTF8
+#if [ $CLIENT_ENCODING = UTF8 ] && [ $SERVER_ENCODING = UTF8 ]; then
+#  mkdir -p -m700 "$METADIST/kvmeta_UTF8"
+#  for TABLE in algorithms checks station_param station types param obs_pgm metadatatype station_metadata model qcx_info operator
+#  do
+#     iconv -f utf-8 -t latin1  $METADIST/kvmeta/$TABLE.out >  $METADIST/kvmeta/$TABLE.latin1
+#     mv $METADIST/kvmeta/$TABLE.out    $METADIST/kvmeta_UTF8/$TABLE.utf8
+#     mv $METADIST/kvmeta/$TABLE.latin1 $METADIST/kvmeta/$TABLE.out
+#  done
+#fi
+#
+#
+#if [[ ( $CLIENT_ENCODING = LATIN1  &&  $SERVER_ENCODING = LATIN1  ) || ( $CLIENT_ENCODING = UTF8 && $SERVER_ENCODING = UTF8 ) ]] ; then
+#   tar cpjf  $kvmetadist kvmeta
+#   cp -pv  kvmeta-$(date +%Y%m%d).tar.bz2  kvmeta.tar.bz2
+#   rm -rf   $METADIST/kvmeta
+#fi
+
+date
+echo "Her begynner det instans spesifikke"
+## INSTANCE dependent metadata, these processes are independent activities
+for INSTANCE in $INSTANCE_LIST METNO
 do
-    assert_table_not_empty $TABLE
-    $PSQL -c "\copy $TABLE to $METADIST/kvmeta/$TABLE.out DELIMITER '|'"
-done
-
-
-assert_table_not_empty station
-$PSQL -c "\copy ( select * from station  where static='t' ) to $METADIST/kvmeta/station.out DELIMITER '|'"
-
-
-cd $METADIST
-kvmetadist=kvmeta-$(date +%Y%m%d).tar.bz2
-
-CLIENT_ENCODING=`$PSQL -tc "SHOW CLIENT_ENCODING"| tr -d ' '`
-SERVER_ENCODING=`$PSQL -tc "SHOW SERVER_ENCODING"| tr -d ' '`
-echo "CLIENT_ENCODING=${CLIENT_ENCODING}"
-echo "SERVER_ENCODING=${SERVER_ENCODING}"
-
-## CHARSET CONVERSION FOR UTF8
-if [ $CLIENT_ENCODING = UTF8 ] && [ $SERVER_ENCODING = UTF8 ]; then
-  mkdir -p -m700 "$METADIST/kvmeta_UTF8"
-  for TABLE in algorithms checks station_param station types param obs_pgm metadatatype station_metadata model qcx_info operator
-  do
-     iconv -f utf-8 -t latin1  $METADIST/kvmeta/$TABLE.out >  $METADIST/kvmeta/$TABLE.latin1
-     mv $METADIST/kvmeta/$TABLE.out    $METADIST/kvmeta_UTF8/$TABLE.utf8
-     mv $METADIST/kvmeta/$TABLE.latin1 $METADIST/kvmeta/$TABLE.out
-  done
-fi
-
-
-if [[ ( $CLIENT_ENCODING = LATIN1  &&  $SERVER_ENCODING = LATIN1  ) || ( $CLIENT_ENCODING = UTF8 && $SERVER_ENCODING = UTF8 ) ]] ; then
-   tar cpjf  $kvmetadist kvmeta
-   cp -pv  kvmeta-$(date +%Y%m%d).tar.bz2  kvmeta.tar.bz2
-   rm -rf   $METADIST/kvmeta
-fi
-
-
-## INSTANCE dependent metadata 
-for INSTANCE in $INSTANCE_LIST
-do
+   (
    if [ -n "$INSTANCE" ]; then
          METADIST_INSTANCE=${METADIST}_${INSTANCE}
    else
          METADIST_INSTANCE=${METADIST}
    fi
 
-   echo "METADIST_INSTANCE=$METADIST_INSTANCE"
+   if [ "$INSTANCE" =  "METNO" ]; then
+         METADIST_INSTANCE=${METADIST}
+   fi
 
+   echo "METADIST_INSTANCE=$METADIST_INSTANCE"
+   
    mkdir -p -m700 "$METADIST_INSTANCE/kvmeta"
    echo "Sjekker antall linjer i tabellene og dumper tabellene"
-   for TABLE in algorithms checks station_param station types param obs_pgm metadatatype station_metadata model qcx_info operator
+   for TABLE in algorithms station types param metadatatype model qcx_info operator
    do
      assert_table_not_empty $TABLE
      $PSQL -c "\copy $TABLE to $METADIST_INSTANCE/kvmeta/$TABLE.out DELIMITER '|'"
    done
 
+   echo "Oppdaterer tabellen obs_pgm for instansen $INSTANCE"
+   #for TABLE in obs_pgm
+   #do
    TABLE="obs_pgm"
+      if [ -n "$INSTANCE" ]; then
+         TABLE_INSTANCE=${TABLE}_${INSTANCE}
+      else
+         TABLE_INSTANCE=${TABLE}
+      fi
+
+      if [ "$INSTANCE" =  "METNO" ]; then
+         TABLE_INSTANCE=${TABLE}
+      fi
+
+      echo "TABLE_INSTANCE=$TABLE_INSTANCE"
+      $PSQL -d $INSTANCE -c "\copy $TABLE to $DUMPDIR/$TABLE_INSTANCE.out DELIMITER '|'"
+       
+      if ! diff -q  $DUMPDIR/$TABLE_INSTANCE.out $METADIR/$TABLE_INSTANCE/$TABLE_INSTANCE.out; then
+         # $LIBEXECDIR/run_$TABLE $INSTANCE
+         $PSQL -d $INSTANCE -a -c "truncate $TABLE"
+         $PSQL -d $INSTANCE -a -c "\copy $TABLE from $METADIR/$TABLE_INSTANCE/$TABLE_INSTANCE.out DELIMITER '|'" 		
+         echo -e `date` "\t$TABLE updated" >> $LOGFILE
+         assert_table_not_empty_instance $INSTANCE $TABLE
+      fi
+
+    assert_table_not_empty_instance $INSTANCE $TABLE
+    $PSQL -d $INSTANCE -c "\copy $TABLE to $METADIST_INSTANCE/kvmeta/$TABLE.out DELIMITER '|'"
+   #done
+
+   TABLE="checks"
+   $LIBEXECDIR/table_instance $INSTANCE $TABLE
    assert_table_not_empty_instance $INSTANCE $TABLE
    $PSQL -d $INSTANCE -c "\copy $TABLE to $METADIST_INSTANCE/kvmeta/$TABLE.out DELIMITER '|'"
-
+   
+   TABLE="station_param"
+   if [ "$INSTANCE" =  "METNO" ]; then
+       # necessary because of speed
+       $LIBEXECDIR/table_instance $INSTANCE $TABLE "$METADIST_INSTANCE/kvmeta"
+       $PSQL -d $INSTANCE -c "\copy $TABLE from $METADIST_INSTANCE/kvmeta/$TABLE.out DELIMITER '|'"
+       assert_table_not_empty_instance $INSTANCE $TABLE
+   else
+       $LIBEXECDIR/table_instance $INSTANCE $TABLE
+       assert_table_not_empty_instance $INSTANCE $TABLE
+       $PSQL -d $INSTANCE -c "\copy $TABLE to $METADIST_INSTANCE/kvmeta/$TABLE.out DELIMITER '|'"
+   fi
+   
+   TABLE="station_metadata"
+   echo "TABLE er station_metadata, instansen  er $INSTANCE"
+   $LIBEXECDIR/table_type_instance $INSTANCE $TABLE
+   if [ "$INSTANCE" =  "METNO" ]; then
+       assert_table_not_empty_instance $INSTANCE $TABLE
+   fi
+   $PSQL -d $INSTANCE -c "\copy $TABLE to $METADIST_INSTANCE/kvmeta/$TABLE.out DELIMITER '|'"
 
    assert_table_not_empty station
    $PSQL -c "\copy ( select * from station  where static='t' ) to $METADIST_INSTANCE/kvmeta/station.out DELIMITER '|'"
-
-
+   
    cd $METADIST_INSTANCE
    kvmetadist=kvmeta_${INSTANCE}-$(date +%Y%m%d).tar.bz2
 
@@ -245,5 +307,6 @@ do
       cp -pv    $kvmetadist  kvmeta_${INSTANCE}.tar.bz2
       rm -rf   $METADIST_INSTANCE/kvmeta
    fi  
-
+   date
+   )&
 done
