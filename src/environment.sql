a446055638635296f15bafeeb9992b64903e8527
Terje Erlend Reite <terje.reite@met.no>
2015-01-20 14:19:04 +0000

    no longer in use
    git-svn-id: https://svn.met.no/kvoss/kvalobs_metadata/trunk@5336 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/environment.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE environment (
-	environmentid INTEGER NOT NULL,
-	alias TEXT NOT NULL,
-	name  TEXT NOT NULL,
-	description TEXT DEFAULT NULL,
-	edited_by    INTEGER NOT NULL,
-	edited_at    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY ( environmentid )
-);
