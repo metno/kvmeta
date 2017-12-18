f8e1b1eb9bcffe3c6dcb0f1f2ecd60c8edb4ddfd
Terje Erlend Reite <terjeer@pc2939.pc.met.no>
2017-12-18 17:01:16 +0100

    extra control

--- src/auto.sh
+++ src/auto.sh
@@ -18,8 +18,19 @@ echo 'Starting /metno/kvalobs/bin/git_pull:'
 echo 'Starting /usr/lib/kvalobs-metadata/bin/run_stinfosys2kvalobs_new:'
 /usr/lib/kvalobs-metadata/bin/run_stinfosys2kvalobs_new
 echo 'Starting /usr/lib/kvalobs-metadata/bin/kvmeta_all:'
-/usr/lib/kvalobs-metadata/bin/kvmeta_all
-echo 'Ending /usr/lib/kvalobs-metadata/bin/kvmeta_all'
+if /usr/lib/kvalobs-metadata/bin/kvmeta_all; then
+   echo 'Ending /usr/lib/kvalobs-metadata/bin/kvmeta_all'
+else
+   for E in error Error ERROR
+   do
+      if grep -q $E $HOME/var/log/run_metadata_instance.log; then
+         echo "$E run_metadata_instance.log" | $HOME/bin/monitor/monitor_message.sh
+         grep $E $HOME/var/log/run_metadata_instance.log
+      fi
+   done
+   echo 'Someting is wrong'
+   exit 1
+fi
 
 if grep -q error $HOME/var/log/run_metadata_instance.log; then
     echo "error run_metadata_instance.log" | $HOME/bin/monitor/monitor_message.sh
